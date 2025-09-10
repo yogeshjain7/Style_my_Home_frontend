0ab1b3523031cdeedb5e4007d7f9eda1fa11d070
yogeshjain7 <167170828+yogeshjain7@users.noreply.github.com>
2025-09-10 19:45:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "furniture-ar-cache-v1";
+const CACHE_NAME = "furniture-ar-cache-v2"; // Incremented version to ensure update
 const ASSETS = [
   "index.html",
   "style.css",
@@ -8,31 +8,36 @@ const ASSETS = [
   // Icons
   "assets/icons/icon-192.png",
   "assets/icons/icon-512.png",
+  
+  // Ad Banners
+  "assets/ads/ad-banner-1.png",
+  "assets/ads/ad-banner-2.png",
+  "assets/ads/ad-banner-3.png",
 
   // Product images
   "assets/images/Arm_Chair.png",
   "assets/images/Brown_L-Sofa.png",
-  "assets/images/Chair.png",
-  "assets/images/Master_Bed.png",
-  "assets/images/Metal_Shelf.png",
-  "assets/images/Night_Stand.png",
-  "assets/images/Shelf.png",
-  "assets/images/Study_Table.png",
-  "assets/images/Table.png",
   "assets/images/Wash_Basin.png",
+  "assets/images/bed.png",
+  "assets/images/table_lamp.png",
+  "assets/images/tv_stand.png",
+  "assets/images/antique_desk.png",
+  "assets/images/bookshelf.png",
+  "assets/images/cupboard.png",
+  "assets/images/coffee_table.png",
   "assets/images/wood_table.png",
 
   // 3D models
   "assets/models/Arm_Chair.glb",
   "assets/models/Brown_L-Sofa.glb",
-  "assets/models/Chair.glb",
-  "assets/models/Master_Bed.glb",
-  "assets/models/Metal_Shelf.glb",
-  "assets/models/Night_Stand.glb",
-  "assets/models/Shelf.glb",
-  "assets/models/Study_Table.glb",
-  "assets/models/Table.glb",
   "assets/models/Wash_Basin.glb",
+  "assets/models/bed.glb",
+  "assets/models/table_lamp.glb",
+  "assets/models/tv_stand.glb",
+  "assets/models/antique_desk.glb",
+  "assets/models/bookshelf.glb",
+  "assets/models/cupboard.glb",
+  "assets/models/coffee_table.glb",
   "assets/models/wood_table.glb"
 ];
 
@@ -40,7 +45,7 @@ const ASSETS = [
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log("Caching assets for offline use");
+      console.log("Caching new assets for offline use");
       return cache.addAll(ASSETS);
     })
   );
@@ -52,7 +57,7 @@ self.addEventListener("activate", event => {
   event.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
